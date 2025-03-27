06ec34979608ba0ae65a0d2db7b82db54d1f0dee
Dumbmoneyk <165833802+Dumbmoneyk@users.noreply.github.com>
2025-03-27 01:49:37 -0500

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Head from "next/head";
 import UtoposLanding from "../components/UtoposLanding";
+import "../styles/globals.css";
 
 export default function Home() {
-  return <UtoposLanding />;
+  return (
+    <>
+      <Head>
+        <title>UTOPOS | Unlock the Codex</title>
+      </Head>
+      <UtoposLanding />
+    </>
+  );
 }
